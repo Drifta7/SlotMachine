0cf4e1ffc9604a80d7254dc3220bb5e5f2bf41c2
Drifta7 <rcdmilla@gmail.com>
2024-08-26 21:05:47 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SlotMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2d = new int[3, 4];
+        
+        
+        
+        
+        }
+    }
+}
