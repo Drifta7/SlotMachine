a3c2c2cc1ceceb4a9990083d2d45edc06c835540
Drifta7 <rcdmilla@gmail.com>
2024-08-28 21:42:32 -0400

    Got the grid printing out random numbers

--- Program.cs
+++ Program.cs
@@ -7,17 +7,17 @@ namespace SlotMachine
         static void Main(string[] args)
         {
             bool quit = false;
-            
+
 
 
 
 
             int[,] Grid2D = new int[3, 3]; // 3x3 grid created 
-         
+
 
 
             Console.Write(" PLace your wager ");
-           
+
             //int wager = 0;
             //Console.ReadLine();
 
@@ -26,15 +26,13 @@ namespace SlotMachine
             const int HIGH = 9; // since 9 is the biggest "ones"
 
             Random range = new Random();
-            int randNumInArray = range.Next(LOW, HIGH);// variable to store the random number 
-            // the nested for loop prints out the game in grid display
-                
-            for (int rows = 0; rows < Grid2D.GetLength(0); rows++) 
+
+            for (int rows = 0; rows < Grid2D.GetLength(0); rows++)
             {
                 for (int cols = 0; cols < Grid2D.GetLength(1); cols++)
                 {
-                     randNumInArray++;
-                    Console.Write(Grid2D[rows, cols]= randNumInArray);    // adding ranNumInArray value into elements
+                    int randNumInArray = range.Next(LOW, HIGH);// variable to store the random number 
+                    Console.Write(Grid2D[rows, cols] = randNumInArray);    // adding ranNumInArray value into elements
                 }
                 Console.WriteLine(); // this makes sure that everthing get printed on the next line.
             }
@@ -45,4 +43,4 @@ namespace SlotMachine
     }
 }
 
-                    //Console.Write(array2d[rows,cols] + " "); // displays the array
+//Console.Write(array2d[rows,cols] + " "); // displays the array
