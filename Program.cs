5f6c64fffaaaa8f64b5b726db48d22c6a68d6275
Drifta7 <rcdmilla@gmail.com>
2024-08-28 21:20:56 -0400

    fixing a bug with the nestedloop on line 36  and 37

--- Program.cs
+++ Program.cs
@@ -9,31 +9,32 @@ namespace SlotMachine
             bool quit = false;
             
 
-            const int LOW = 0; // const low number 
-            int HIGH = 9; // since 9 is the biggest "ones"
-
-            Random range = new Random();
-            int randNumInArray = range.Next(LOW, HIGH);// variable to store the random number 
 
 
 
-            int[,] array2d = new int[3, 3]; // 3x3 grid created 
+            int[,] Grid2D = new int[3, 3]; // 3x3 grid created 
          
 
 
             Console.Write(" PLace your wager ");
-            Console.WriteLine(randNumInArray); // (for testing purposes)
-            int wager = 0;
-            Console.ReadLine();
+           
+            //int wager = 0;
+            //Console.ReadLine();
 
 
+            const int LOW = 0; // const low number 
+            const int HIGH = 9; // since 9 is the biggest "ones"
+
+            Random range = new Random();
+            int randNumInArray = range.Next(LOW, HIGH);// variable to store the random number 
             // the nested for loop prints out the game in grid display
-            for (int rows = 0; rows < array2d.GetLength(0); rows++) 
+                
+            for (int rows = 0; rows < Grid2D.GetLength(0); rows++) 
             {
-                for (int cols = 0; cols <array2d.GetLength(1); cols++)
+                for (int cols = 0; cols < Grid2D.GetLength(1); cols++)
                 {
-                    Console.Write(array2d[rows,cols] + " "); // displays the array
-                    Console.Write(array2d[rows, cols] = randNumInArray);    // adding ranNumInArray value into elements
+                     randNumInArray++;
+                    Console.Write(Grid2D[rows, cols]= randNumInArray);    // adding ranNumInArray value into elements
                 }
                 Console.WriteLine(); // this makes sure that everthing get printed on the next line.
             }
@@ -43,3 +44,5 @@ namespace SlotMachine
         }
     }
 }
+
+                    //Console.Write(array2d[rows,cols] + " "); // displays the array
